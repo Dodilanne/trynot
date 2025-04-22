7dadb74edb0ef7626439ddb6cce493552113f12b
Dodilanne <dodilanne@ada-anvil.io>
2025-04-22 11:25:48 -0400

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export { getFailureReason } from "./get-failure-reason";
 export { isErr } from "./is-err";
 export { isOk } from "./is-ok";
 export type { Result } from "./result";
+export { unwrapOrElse } from "./unwrap-or-else";
+export { unwrapOrUndefined } from "./unwrap-or-undefined";
+export { unwrapOr } from "./unwrap-or";
+export { unwrap } from "./unwrap";
